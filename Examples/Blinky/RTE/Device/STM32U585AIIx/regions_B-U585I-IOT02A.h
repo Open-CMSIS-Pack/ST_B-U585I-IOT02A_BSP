d31212898764b0478663d8485db97ea69a11c707
Ziga Mahne <ziga.mahne@arm.com>
2025-09-25 08:58:16 +0200

    Update memory regions files

--- Examples/Blinky/RTE/Device/STM32U585AIIx/regions_B-U585I-IOT02A.h
+++ Examples/Blinky/RTE/Device/STM32U585AIIx/regions_B-U585I-IOT02A.h
@@ -6,12 +6,12 @@
 //------ With VS Code: Open Preview for Configuration Wizard -------------------
 
 // <n> Auto-generated using information from packs
-// <i> Device Family Pack (DFP):   Keil::STM32U5xx_DFP@3.0.0
-// <i> Board Support Pack (BSP):   Keil::B-U585I-IOT02A_BSP@2.0.0
+// <i> Device Family Pack (DFP):   Keil::STM32U5xx_DFP@3.2.0
+// <i> Board Support Pack (BSP):   Keil::B-U585I-IOT02A_BSP@3.0.0
 
 // <h> ROM Configuration
 // =======================
-// <h> __ROM0 (is rx memory: Flash from DFP)
+// <h> __ROM0 (is rx memory: Flash_NS from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region. Default: 0x08000000
 //   <i> Contains Startup and Vector Table
@@ -24,75 +24,79 @@
 // <h> __ROM1 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM1_BASE 0
+#define __ROM1_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM1_SIZE 0
+#define __ROM1_SIZE 0x0
 // </h>
 
 // <h> __ROM2 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM2_BASE 0
+#define __ROM2_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM2_SIZE 0
+#define __ROM2_SIZE 0x0
 // </h>
 
 // <h> __ROM3 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM3_BASE 0
+#define __ROM3_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM3_SIZE 0
+#define __ROM3_SIZE 0x0
 // </h>
 
 // </h>
 
 // <h> RAM Configuration
 // =======================
-// <h> __RAM0 (is rwx memory: SRAM1_2 from DFP)
+// <h> __RAM0 (is rwx memory: SRAM1_NS+SRAM2_NS+SRAM3_NS from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region. Default: 0x20000000
 //   <i> Contains uninitialized RAM, Stack, and Heap
 #define __RAM0_BASE 0x20000000
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
-//   <i> Defines size of memory region. Default: 0x00040000
-#define __RAM0_SIZE 0x00040000
+//   <i> Defines size of memory region. Default: 0x000C0000
+#define __RAM0_SIZE 0x000C0000
 // </h>
 
-// <h> __RAM1 (unused)
+// <h> __RAM1 (is rwx memory: SRAM4_NS from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
-//   <i> Defines base address of memory region.
-#define __RAM1_BASE 0
+//   <i> Defines base address of memory region. Default: 0x28000000
+#define __RAM1_BASE 0x28000000
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
-//   <i> Defines size of memory region.
-#define __RAM1_SIZE 0
+//   <i> Defines size of memory region. Default: 0x00004000
+#define __RAM1_SIZE 0x00004000
 // </h>
 
 // <h> __RAM2 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __RAM2_BASE 0
+#define __RAM2_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __RAM2_SIZE 0
+#define __RAM2_SIZE 0x0
 // </h>
 
 // <h> __RAM3 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __RAM3_BASE 0
+#define __RAM3_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __RAM3_SIZE 0
+#define __RAM3_SIZE 0x0
 // </h>
 
 // </h>
 
 // <n> Resources that are not allocated to linker regions
-// <i> rwx RAM:  SRAM3 from DFP:           BASE: 0x20040000  SIZE: 0x00080000
+// <i> rx ROM:   Flash_S from DFP:         BASE: 0x0C000000  SIZE: 0x00200000
+// <i> rwx RAM:  SRAM1_S from DFP:         BASE: 0x30000000  SIZE: 0x00030000
+// <i> rwx RAM:  SRAM2_S from DFP:         BASE: 0x30030000  SIZE: 0x00010000
+// <i> rwx RAM:  SRAM3_S from DFP:         BASE: 0x30040000  SIZE: 0x00080000
+// <i> rwx RAM:  SRAM4_S from DFP:         BASE: 0x38000000  SIZE: 0x00004000
 // <i> rwx RAM:  RAM-External from BSP:    BASE: 0x90000000  SIZE: 0x00800000
 // <i> rx ROM:   Flash-External from BSP:  BASE: 0x70000000  SIZE: 0x04000000
 
